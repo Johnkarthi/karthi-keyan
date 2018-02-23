e0fbfa6aa9c1b5fd485162f06d0a61a76760542f
Johnkarthi <johnkarthi65@gmail.com>
2018-02-23 09:18:15 +0530

    Create 52.py

---
+++ 52.py
@@ -0,0 +1,21 @@
+x=input()
+if(x==1):
+    print("ONE")
+elif(x==2):
+    print("TWO")
+elif(x==3):
+    print("THREE")
+elif(x==4):
+    print("FOUR")
+elif(x==5):
+    print("FIVE")
+elif(x==6):
+    print("SIX")
+elif(x==7):
+    print("SEVEN")
+elif(x==8):
+    print("EIGHT")
+elif(x==9):
+    print("NINE")
+else:
+    print("TEN")
