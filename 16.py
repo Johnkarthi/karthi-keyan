7e3c6f737887f0887dfd7fc8c5273cac78f8f92a
Johnkarthi <johnkarthi65@gmail.com>
2018-03-14 16:18:01 +0530

    Create 16.py

---
+++ 16.py
@@ -0,0 +1,10 @@
+a = input()
+b= input()
+
+for num in range(a,b + 1):
+   if num > 1:
+       for i in range(2,num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
