73276b034f8e1b191325708636acb8b8b4474537
Johnkarthi <johnkarthi65@gmail.com>
2018-03-20 16:10:06 +0530

    Update 25.c

--- 25.c
+++ 25.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 int main()
 {
-    int a[10];
+    int x[10];
     int n;
     int i, j, temp;
             scanf("%d",&n);
     for(i=0;i<n;i++)
     {
-        scanf("%d",&a[i]);
+        scanf("%d",&x[i]);
     }
 
     for(i=0;i<n;i++)
     {
             for(j=i;j<n;j++)
         {
-                if(a[i]>a[j])
+                if(x[i]>x[j])
             {
-                temp=a[i];
-                a[i]=a[j];
-                a[j] = temp;
+                temp=x[i];
+                x[i]=x[j];
+                x[j] = temp;
             }
         }
     }
