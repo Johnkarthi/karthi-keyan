3e0b9e05af44181d5b2323c454854efa1b707c8a
Johnkarthi <johnkarthi65@gmail.com>
2018-03-20 15:38:43 +0530

    Update 20.c

--- 20.c
+++ 20.c
@@ -2,15 +2,15 @@
 # include <conio.h>
 void main() 
 { 
- int i,num,div ; 
+ int x,y,div ; 
  clrscr() ; 
  printf("Enter the limit : ") ; 
- scanf("%d", &num) ; 
+ scanf("%d", &y) ; 
  printf("\nEnter the number : ") ; 
  scanf("%d", &div) ; 
  printf("\nThe numbers divisible by %d are :\n\n", div) ; 
- for(i = 1 ; i <= num ; i++) 
-  if(i % div == 0) 
-   printf("%d\t", i) ; 
+ for(x = 1 ; x <= y ; x++) 
+  if(x % div == 0) 
+   printf("%d\t", x) ; 
  getch() ; 
 }
