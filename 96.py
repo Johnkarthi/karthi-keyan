75e80e8524bebfc2cd60fce147cfdbe8e4bbe45b
Johnkarthi <johnkarthi65@gmail.com>
2018-03-21 16:29:32 +0530

    Create 96.py

---
+++ 96.py
@@ -0,0 +1,12 @@
+n = int(input("Enter any number : "))
+if n> 1:
+    for i in range(2, n):
+        if (n % i) == 0:
+            print("Yes")
+            break
+    else:
+        print("No")
+elif n == 0 or 1:
+    print(" a neither prime NOR composite number")
+else:
+    print("Yes")
