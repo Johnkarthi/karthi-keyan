bee1883a07624572004e7fb4141b9172a7c98c0a
Johnkarthi <johnkarthi65@gmail.com>
2018-03-21 16:10:35 +0530

    Create 85.py

---
+++ 85.py
@@ -0,0 +1,7 @@
+xx=input("Enter a string")
+list1=[]
+list2=[]
+for i in range(len(xx)):
+    if i%2==0:list1.append(s[i])
+    else:list2.append(s[i])
+print("".join(str(x) for x in list1),"".join(str(x) for x in list2))
