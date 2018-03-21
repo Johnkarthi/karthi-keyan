fb8678be8633610908dbe9aabc4b94426881f9e5
Johnkarthi <johnkarthi65@gmail.com>
2018-03-21 15:34:06 +0530

    Update 34.py

--- 34.py
+++ 34.py
@@ -1,6 +1,6 @@
-a = input("Enter file name: ")
-b = 0
-with open(a, 'r') as f:
+x = input("Enter file name: ")
+y = 0
+with open(x, 'r') as f:
     for c in f:
-        b += 1
-print(b)
+        y += 1
+print(y)
