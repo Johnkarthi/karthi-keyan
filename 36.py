980a60ebf75d8614efced0831438ef40ad3251eb
Johnkarthi <johnkarthi65@gmail.com>
2018-03-21 15:40:26 +0530

    Update 36.py

--- 36.py
+++ 36.py
@@ -1,12 +1,12 @@
 #include<stdio.h>
 void main()
 {
-char str[50],i,c=0;
+char str[50],x,c=0;
 printf("enter the word");
 gets(str);
-for(i=0;str[i]!='\0';i++)
+for(x=0;str[x]!='\0';i++)
 {
-if((str[i]>='0')&&(str[i]<='9')||(str[i]>='a')&&(str[i]<='z'))
+if((str[x]>='0')&&(str[x]<='9')||(str[x]>='a')&&(str[x]<='z'))
 {
 continue;
 }
