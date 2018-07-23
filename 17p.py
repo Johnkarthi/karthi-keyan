8039699b721020496f349ca435250b76cbce0cfa
karthi keyan.k <johnkarthi65@gmail.com>
2018-07-23 14:09:40 +0530

    Update 17p.py

--- 17p.py
+++ 17p.py
@@ -1,8 +1,8 @@
-def gcd(a,b):
+def gcd(x,b):
     while b > 0:
-        a, b = b, a % b
-    return a
-def lcm(a, b):
-    return a * b / gcd(a, b)
-a,b=map(int,input("Enter the values:").split(' '))
-print(int(lcm(a,b)))
+        x, b = b, x % b
+    return x
+def lcm(x, b):
+    return x * b / gcd(x, b)
+x,b=map(int,input("Enter the values:").split(' '))
+print(int(lcm(x,b)))
