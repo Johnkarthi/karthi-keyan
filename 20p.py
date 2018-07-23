e6b79558b78df627f01966f8941acc24770538e1
karthi keyan.k <johnkarthi65@gmail.com>
2018-07-23 14:22:52 +0530

    Update 20p.py

--- 20p.py
+++ 20p.py
@@ -2,9 +2,9 @@ def main():
     str=input()
     list=[]
     for i in str:
-        x=ord(i)+3
-        y=chr(n)
-        list.append(y)
+        p=ord(i)+3
+        q=chr(n)
+        list.append(q)
 
     print(''.join(list))
 if __name__ == '__main__':
