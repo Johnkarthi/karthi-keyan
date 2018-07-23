4fb6f1b174b6a8b1e9cf7fc616cd3ad7a056e91c
karthi keyan.k <johnkarthi65@gmail.com>
2018-07-23 10:53:27 +0530

    Update 6p.py

--- 6p.py
+++ 6p.py
@@ -1,26 +1,26 @@
 try:
 	s1=str(raw_input())
 	s2=str(raw_input())
-	a=[]
-	b=[]
-	d=[]
+	x=[]
+	y=[]
+	z=[]
 	c=0
 	n=len(s1)
 	for i in range(0,n):
-		if s1[i] in a :
-			b.append(i)
+		if s1[i] in x :
+			y.append(i)
 			c=i
 		else:
-			a.append(s1[i])
+			x.append(s1[i])
 	for i in range(0,n):
 		if(s1[i]==s1[c]):
-			d.append(i)
+			z.append(i)
 		else: 
 			pass
-	x=len(d)-1
-	while(x!=0):
-		i=d[x]
-		j=d[x-1]
+	a=len(c)-1
+	while(a!=0):
+		i=c[x]
+		j=c[x-1]
 		if (s2[i]==s2[j]):
 			print "true"
 		else:
